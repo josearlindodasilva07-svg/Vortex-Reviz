4569d0b3a083e82d42c297cf882845bd49688f45
josearlindodasilva07-svg <josearlindodasilva07@gmail.com>
2026-02-27 21:08:39 -0300

    Update notification titles and texts for Vortex Reviz

--- VortexReviz.lua
+++ VortexReviz.lua
@@ -663,14 +663,14 @@ lplayer.Chatted:Connect(function(msg)
         if game:GetService("Workspace").FilteringEnabled == true then
             warn("FE is Enabled (Filtering Enabled)")
             game:GetService("StarterGui"):SetCore("SendNotification", {
-                Title = "FE is Enabled";
-                Text = "Filtering Enabled. Enjoy using Reviz Admin!";
+                Title = "Vortex Reviz";
+                Text = "Vortex Reviz Carregando";
             })
         else
             warn("FE is Disabled (Filtering Disabled) Consider using a different admin script.")
             game:GetService("StarterGui"):SetCore("SendNotification", {
-                Title = "FE is Disabled";
-                Text = "Filtering Disabled. Consider using a different admin script.";
+                Title = "Vortex Reviz";
+                Text = "Vortex Reviz Carregando.";
             })
         end
     end
